67d2aee4d5c8c45dadb425d785b955cbea6d6445
Avinash Halsnad <avinashh2@gmail.com>
2016-11-20 16:57:37 +0100

    No going back without valid user.

--- src/main/java/cloud/Noback.java
+++ src/main/java/cloud/Noback.java
@@ -10,20 +10,11 @@ import javax.servlet.http.HttpSession;
 
 public class Noback {
 	public void noback(HttpSession session, HttpServletRequest request, HttpServletResponse response) {
-		response.setHeader("Cache-Control", "no-cache"); // Forces caches to
-															// obtain a new copy
-															// of the page from
-															// the origin server
-		response.setHeader("Cache-Control", "no-store"); // Directs caches not
-															// to store the page
-															// under any
-															// circumstance
-		response.setDateHeader("Expires", 0); // Causes the proxy cache to see
-												// the page as "stale"
-		response.setHeader("Pragma", "no-cache"); // HTTP 1.0 backward
-													// compatibility
+		response.setHeader("Cache-Control", "no-cache"); 
+		response.setHeader("Cache-Control", "no-store"); 
+		response.setDateHeader("Expires", 0); 
+		response.setHeader("Pragma", "no-cache"); 
 		String userName = (String) session.getAttribute("user");
-		String usermail = (String) session.getAttribute("email");
 		if (null == userName) {
 			request.setAttribute("Error", "Session has ended.  Please login.");
 			RequestDispatcher rd = request.getRequestDispatcher("/index.jsp");
