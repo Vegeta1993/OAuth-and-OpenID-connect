91bfe6d40d9f8fdb889833b2581f6a9a8965ffd1
Avinash Halsnad <avinashh2@gmail.com>
2016-11-22 18:34:52 +0100

    Changed URL to server IP.

--- src/main/java/com/project1/Logout.java
+++ src/main/java/com/project1/Logout.java
@@ -28,42 +28,61 @@ import sun.net.www.http.HttpClient;
  */
 public class Logout extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	
-    public Logout() {
-        super();
-    }
-    
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		HttpSession session= request.getSession(false);
-		if(session.getAttribute("realm").toString().equals("google")){
-			String at = session.getAttribute("at").toString();
-			String redirect_url = "http://localhost:8080/project1/logout.jsp";
-			String returnValue = "https://www.google.com/accounts/Logout?continue=https://appengine.google.com/_ah/logout?continue="+redirect_url;
-			try{
-				 response.sendRedirect(returnValue);
-				 return;
-		} catch (Exception e) {
-			// an error occurred, handle this
-			e.printStackTrace();
-		}
-			
-		}
-		session.removeAttribute("user");
-		session.removeAttribute("uid");
-		session.invalidate();    
-		Cookie[] cookies = request.getCookies();
-	    for (Cookie cookie : cookies) {
-	        cookie.setMaxAge(0);
-	        cookie.setValue(null);
-	        cookie.setPath("/");
-	        response.addCookie(cookie);
-	    }
-		RequestDispatcher rd = request.getRequestDispatcher("/index.jsp"); 
-        rd.forward(request,response);
+
+	public Logout() {
+		super();
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		HttpSession session = request.getSession(false);
+		String realm = session.getAttribute("realm").toString();
+		if (realm.equals("google")) {
+			String at = session.getAttribute("at").toString();
+			String redirect_url = "http://192.168.12.16.nip.io:8080/project1/logout.jsp";
+			String returnValue = "https://www.google.com/accounts/Logout?continue=https://appengine.google.com/_ah/logout?continue="
+					+ redirect_url;
+			try {
+				response.sendRedirect(returnValue);
+				return;
+			} catch (Exception e) {
+				// an error occurred, handle this
+				e.printStackTrace();
+			}
+		}
+		if (realm.equals("github")) {
+			String clid = session.getAttribute("clid").toString();
+			String returnValue1 = "https://github.com/settings/applications/440899#revoke_tokens";
+			try {
+				response.sendRedirect(returnValue1);
+				return;
+			} catch (Exception e) {
+				// an error occurred, handle this
+				e.printStackTrace();
+			}
+
+		}
+
+		session.removeAttribute("realm");
+		session.removeAttribute("email");
+		session.removeAttribute("at");
+		session.removeAttribute("user");
+		session.removeAttribute("uid");
+		session.invalidate();
+		Cookie[] cookies = request.getCookies();
+		for (Cookie cookie : cookies) {
+			cookie.setMaxAge(0);
+			cookie.setValue(null);
+			cookie.setPath("/");
+			response.addCookie(cookie);
+		}
+		RequestDispatcher rd = request.getRequestDispatcher("/index.jsp");
+		rd.forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 
 		doGet(request, response);
 	}
