a088006902bb8991baa3a6c803f879717de65f55
Avinash Halsnad <avinashh2@gmail.com>
2016-11-22 18:34:34 +0100

    Changed URL to server IP.

--- src/main/java/com/project1/DeleteChat.java
+++ src/main/java/com/project1/DeleteChat.java
@@ -21,56 +21,54 @@ import com.db.User;
  */
 public class DeleteChat extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-    public DeleteChat() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	public DeleteChat() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		String uid = request.getParameter("userid");
-		HttpSession session1 = request.getSession(false);
-		String uid1=session1.getAttribute("uid").toString();
-		if(uid.equals(uid1)==false){
-			response.sendRedirect("http://localhost:8080/project1/Error.jsp");
-			return;
-		}
-		int uid2 = Integer.parseInt(uid1);
-		int chatId = Integer.parseInt(request.getParameter("chatID"));
+
 		Session session = null;
-        Transaction tx = null;
-        
-        try 
-        {
-        	Connector conn = new Connector();
-            session = conn.configureSessionFactory().openSession();
-            tx = session.beginTransaction();
-            
-            String hsql = "delete from Chat where id = :chatID and user_id = :uid";
-            Query query = session.createQuery(hsql);
-            query.setParameter("chatID", chatId);
-            query.setParameter("uid", uid2);
-            int res = query.executeUpdate();
-            
-            
-        } 
-        catch (Exception ex) 
-        {
-        	tx.rollback();
-            ex.printStackTrace();
-        } 
-        finally
-        {
-        	if(session != null && session.isOpen())
-			{
-        		tx.commit();
+		Transaction tx = null;
+
+		try {
+			Connector conn = new Connector();
+			session = conn.configureSessionFactory().openSession();
+			tx = session.beginTransaction();
+			HttpSession session1 = request.getSession(false);
+			String uid1 = session1.getAttribute("uid").toString();
+			if (uid.equals(uid1) == false) {
+				response.sendRedirect("/Error.jsp");
+				return;
+			}
+			int uid2 = Integer.parseInt(uid1);
+			int chatId = Integer.parseInt(request.getParameter("chatID"));
+			String hsql = "delete from Chat where id = :chatID and user_id = :uid";
+			Query query = session.createQuery(hsql);
+			query.setParameter("chatID", chatId);
+			query.setParameter("uid", uid2);
+			int res = query.executeUpdate();
+
+		} catch (Exception ex) {
+			tx.rollback();
+			ex.printStackTrace();
+		} finally {
+			if (session != null && session.isOpen()) {
+				tx.commit();
 				session.flush();
 				session.close();
 			}
-        	
-        	response.sendRedirect("messageBoard.jsp");
-        }
-		
+
+			response.sendRedirect("messageBoard.jsp");
+		}
+
 	}
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
