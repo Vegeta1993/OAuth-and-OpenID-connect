0835b3f455bcabe02f8348e98a689a616bcbbf0d
Avinash Halsnad <avinashh2@gmail.com>
2016-11-22 18:35:01 +0100

    Changed URL to server IP.

--- src/main/java/com/project1/ReturnGoogle.java
+++ src/main/java/com/project1/ReturnGoogle.java
@@ -55,57 +55,55 @@ public class ReturnGoogle extends HttpServlet {
 		super();
 	}
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		HttpSession session1=request.getSession();
-		session1.setAttribute("realm", "google");
-		String returnURL="http://localhost:8080/project1/ReturnGoogle";
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		String returnURL = "http://192.168.12.16.nip.io:8080/project1/ReturnGoogle";
 		String query1 = request.getRequestURL().append("?").append(request.getQueryString()).toString();
-		URI s=null;
+		URI s = null;
 		try {
 			s = new URI(query1);
 		} catch (URISyntaxException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		AuthenticationResponse authResponse=null;
+		AuthenticationResponse authResponse = null;
 		try {
 			authResponse = AuthenticationResponseParser.parse(s);
 		} catch (ParseException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		if(authResponse instanceof AuthenticationErrorResponse) {
-		// TODO: error handling: authentication error
+		if (authResponse instanceof AuthenticationErrorResponse) {
+			// TODO: error handling: authentication error
 		}
-		
-			  
-		AuthenticationSuccessResponse authzSuccess =(AuthenticationSuccessResponse)authResponse;
-		AuthorizationCode code =authzSuccess.getAuthorizationCode();
+
+		AuthenticationSuccessResponse authzSuccess = (AuthenticationSuccessResponse) authResponse;
+		AuthorizationCode code = authzSuccess.getAuthorizationCode();
 		com.nimbusds.oauth2.sdk.id.State state = authzSuccess.getState();
-		String state1=state.toString();
-		String state2=request.getSession().getAttribute("state").toString();
-		response.getWriter().print(state1 + " " + state2);
-		if (state1.equals(state2)==false) {
-			    response.getWriter().print("State not equal");
-			    return;
-		}
-		
-		ClientID clientID =new ClientID("664700022174-tkgm8ehfjl4sieruvsi1chqkassg6n6p.apps.googleusercontent.com");
+//		String state1 = state.toString();
+//		String state2 = request.getSession().getAttribute("state").toString();
+//		response.getWriter().print(state1 + " " + state2);
+//		if (state1.equals(state2) == false) {
+//			response.getWriter().print("CSRF attack attempted");
+//			return;
+//		}
+
+		ClientID clientID = new ClientID("664700022174-tkgm8ehfjl4sieruvsi1chqkassg6n6p.apps.googleusercontent.com");
 		Secret clientSecret = new Secret("HqQRmVe9AEbzBeciRsnPnaks");
-		ClientAuthentication clientAuth =new ClientSecretPost(clientID, clientSecret);
-		
-		URI tokenEndpointURL=null;
+		ClientAuthentication clientAuth = new ClientSecretPost(clientID, clientSecret);
+
+		URI tokenEndpointURL = null;
 		try {
 			tokenEndpointURL = new URI("https://www.googleapis.com/oauth2/v4/token");
 		} catch (URISyntaxException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		TokenRequest accessTokenRequest=null;
+		TokenRequest accessTokenRequest = null;
 		try {
-			accessTokenRequest = new TokenRequest(
-			tokenEndpointURL, clientAuth,new AuthorizationCodeGrant(code,new URI(returnURL)));
+			accessTokenRequest = new TokenRequest(tokenEndpointURL, clientAuth,
+					new AuthorizationCodeGrant(code, new URI(returnURL)));
 		} catch (URISyntaxException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -113,8 +111,8 @@ public class ReturnGoogle extends HttpServlet {
 		HTTPRequest httpRequest;
 		httpRequest = accessTokenRequest.toHTTPRequest();
 		HTTPResponse httpResponse = httpRequest.send();
-		
-		TokenResponse tokenResponse=null;
+
+		TokenResponse tokenResponse = null;
 		try {
 			tokenResponse = OIDCTokenResponseParser.parse(httpResponse);
 		} catch (ParseException e) {
@@ -122,107 +120,89 @@ public class ReturnGoogle extends HttpServlet {
 			e.printStackTrace();
 		}
 		if (tokenResponse instanceof TokenErrorResponse) {
-				// do error handling
-			
-				TokenErrorResponse tokenError=(TokenErrorResponse)tokenResponse;
-				response.getWriter().println("Error on receiving token"+ tokenError);
-				return;
+			// do error handling
+
+			TokenErrorResponse tokenError = (TokenErrorResponse) tokenResponse;
+			response.getWriter().println("Error on receiving token" + tokenError);
+			return;
 		}
-		OIDCTokenResponse tokenSuccess =(OIDCTokenResponse)tokenResponse;
+		OIDCTokenResponse tokenSuccess = (OIDCTokenResponse) tokenResponse;
 		BearerAccessToken accessToken = (BearerAccessToken) tokenSuccess.getOIDCTokens().getBearerAccessToken();
 		RefreshToken refreshToken = tokenSuccess.getOIDCTokens().getRefreshToken();
 		JWT idToken = tokenSuccess.getOIDCTokens().getIDToken();
-				// just for testing:
-		net.minidev.json.JSONObject jsonObject=null;
+		// just for testing:
+		net.minidev.json.JSONObject jsonObject = null;
 		try {
 			jsonObject = idToken.getJWTClaimsSet().toJSONObject();
 		} catch (java.text.ParseException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		String email = jsonObject.get("email").toString();
 		String name = jsonObject.get("name").toString();
-		session1.setAttribute("user", email);
-		session1.setAttribute("email", email);
-		session1.setAttribute("at", accessToken);
 
-		String realm = "GOOGLE"; //Change this parameter based what you need
-		int uid=0;
-		
+		/* ---- CHECK TO OUR DB BEGIN ---- */
+		String realm = "GOOGLE"; // Change this parameter based what you need
+		int uid = 0;
+
 		Session session = null;
-        Transaction tx = null;
-		try 
-		{
+		Transaction tx = null;
+		try {
 			Connector conn = new Connector();
-            session = conn.configureSessionFactory().openSession();
-            tx = session.beginTransaction();
-            
-            String hsql = "from User where email = :user_email and realm = :login_realm";
-            Query query = session.createQuery(hsql);
-            query.setParameter("user_email", email);
-            query.setParameter("login_realm", realm);
-            List<User> result = query.list();
-            
-            if(result.size() != 0)
-            {
-            	//USER IS EXIST, DO THE AUTHORIZATION METHOD
-            	for(User u : result)
-            	{
-            		 System.out.println("Id: " + u.getId() + " | Name:"  + u.getName() + " | Email:" + u.getEmail() + " | Realm:" + u.getRealm());
-            		 uid= u.getId();
-            		 session1.setAttribute("uid", uid);
-        		 	/**
-        			 * 
-        			 * 
-        			 * DO YOUR AUTHORIZATION MAGIC HERE
-        			 * 
-        			 * 
-        			 */
-            	}
-            }
-            else
-            {
-            	//USER IS NOT EXIST, STORE IN DB
-            	try
-            	{
-            		User newUser = new User(uid, name, email,realm);
-            		session.save(newUser);
-            		uid=newUser.getId(); 
-            		session1.setAttribute("uid", uid);
+			session = conn.configureSessionFactory().openSession();
+			tx = session.beginTransaction();
 
-            	}
-            	catch(Exception ex)
-            	{
-            		ex.printStackTrace();
-            		tx.rollback();
-            	}
-            	
-            	/**
-    			 * 
-    			 * 
-    			 * DO YOUR AUTHORIZATION MAGIC HERE
-    			 * 
-    			 * 
-    			 */
-            }
-        } 
-		catch (Exception ex) 
-		{
-			
-            ex.printStackTrace();
-        } 
-		finally
-		{
-			if(session != null && session.isOpen())
-			{
+			String hsql = "from User where email = :user_email and realm = :login_realm";
+			Query query = session.createQuery(hsql);
+			query.setParameter("user_email", email);
+			query.setParameter("login_realm", realm);
+			List<User> result = query.list();
+
+			if (result.size() != 0) {
+				// USER IS EXIST, DO THE AUTHORIZATION METHOD
+				for (User u : result) {
+					System.out.println("Id: " + u.getId() + " | Name:" + u.getName() + " | Email:" + u.getEmail()
+							+ " | Realm:" + u.getRealm());
+					uid = u.getId();
+					HttpSession session1 = request.getSession(true);
+					session1.setAttribute("realm", "google");
+					session1.setAttribute("user", email);
+					session1.setAttribute("email", email);
+					session1.setAttribute("at", accessToken);
+					session1.setAttribute("uid", uid);
+				}
+			} else {
+				// USER IS NOT EXIST, STORE IN DB
+				try {
+					User newUser = new User(uid, name, email, realm);
+					session.save(newUser);
+					uid = newUser.getId();
+
+				} catch (Exception ex) {
+					ex.printStackTrace();
+					tx.rollback();
+				}
+
+				HttpSession session1 = request.getSession(true);
+				session1.setAttribute("realm", "google");
+				session1.setAttribute("user", email);
+				session1.setAttribute("email", email);
+				session1.setAttribute("at", accessToken);
+				session1.setAttribute("uid", uid);
+			}
+		} catch (Exception ex) {
+			response.getWriter().println(ex.getMessage());
+			ex.printStackTrace();
+		} finally {
+			if (session != null && session.isOpen()) {
 				tx.commit();
 				session.flush();
 				session.close();
 			}
 			response.sendRedirect("messageBoard.jsp");
-        }
-		/* ---- CHECK TO OUR DB END ----*/
+		}
+		/* ---- CHECK TO OUR DB END ---- */
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
